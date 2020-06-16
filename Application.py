585f8b5525775e2eea0d764d831b285d0e3a2b3f
chblaze@gmail.com <candjblaze@gmail.com>
2020-06-16 14:06:46 -0500

    starting website for it

--- Application.py
+++ Application.py
@@ -1,6 +1,34 @@
 import EbayScrapper
+import AmazonScrapper
+import concurrent.futures
+import time
 
-scrapper = EbayScrapper.EbayWebScrapper()
+amazonExecutor = concurrent.futures.ThreadPoolExecutor(max_workers = 1)
+ebayExecutor = concurrent.futures.ThreadPoolExecutor(max_workers = 1)
 
-scrapper.search("Air Jordans")
+ebayScrapper = EbayScrapper.EbayWebScrapper()
+amazonScrapper = AmazonScrapper.AmazonScrapper()
 
+amazonFuture = amazonExecutor.submit( amazonScrapper.priceSearch, "Air Jordans" )
+ebayFuture = ebayExecutor.submit( ebayScrapper.priceSearch, "Air Jordans" )
+
+while(amazonFuture.running() or ebayFuture.running()):
+    time.sleep(1)
+
+amazonItems = amazonFuture.result()
+ebayItems = ebayFuture.result()
+
+ebayScrapper.finish()
+amazonScrapper.finish()
+
+amazonItems.update(ebayItems)
+
+sortedItems = sorted(amazonItems.items(), key=lambda item: item[1])
+
+sortedDict = {}
+for item in sortedItems:
+    sortedDict[item[0]] = item[1]
+
+print("The Final combined list of items is as follows")
+for item in sortedDict:
+    print(item + " Priced at " + str(sortedDict[item]) )
--- EbayScrapper.py
+++ EbayScrapper.py
@@ -1,4 +1,5 @@
-# import time
+import time
+import collections
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,40 +8,60 @@ from webdriver_manager.chrome import ChromeDriverManager
 class EbayWebScrapper:
 
     url = "https://www.ebay.com/"
-    inputSearchElementXpath = "//input[class='gh-tb ui-autocomplete-input']"
-    searchButtonElementXpath = "//input[type='submit'][class='btn btn-prim gh-spr'][id='gh-btn'][value='Search']"
-    listDisplayElementXpath = "//ul[class='srp-river-results']"
-    itemElementXpath = "/li[class='s-item']"
+    inputSearchElementXpath = "//input[@class='gh-tb ui-autocomplete-input']"
+    searchButtonElementXpath = "//input[@type='submit'][@class='btn btn-prim gh-spr'][@id='gh-btn'][@value='Search']"
+    listDisplayElementXpath = "//ul[@class='srp-results srp-grid clearfix']"
+    itemElementXpath = "//*[@id='srp-river-results']//ul//li[@class='s-item    '][@data-view='mi:1686|iid:"
+    itemPriceXpath = "//span[@class='s-item__price']"
+    itemNameXpath = "//h3"
+    maxRetries = 3
 
 
     def __init__(self):
         self.myDriver = webdriver.Chrome(ChromeDriverManager().install())
-        print("current session is "+self.myDriver.session_id)
         self.myDriver.get(self.url)
-        print("Initializted self")
+        print("Initializted ebay Scrapper")
 
-    def search(self, searchText):
-        print("Entering in the search info of "+searchText)
+    def priceSearch(self, searchText):
+
+        header = self.myDriver.find_element_by_tag_name("header")
         try:
-            self.myDriver.find_element_by_xpath(self.inputSearchElementXpath).sendKeys(searchText)
-            self.myDriver.find_element_by_xpath(self.searchButtonElementXpath).click()
+            searchBox = header.find_element_by_xpath(self.inputSearchElementXpath)
+            searchBox.send_keys(searchText)
+            searchButton = header.find_element_by_xpath(self.searchButtonElementXpath)
+            searchButton.click()
         except NoSuchElementException as e:
             print(e)
             print("Failed to enter into the text box and press the button")
 
-        try:
-            itemElement = self.myDriver.find_element_by_xpath(self.listDisplayElementXpath)
-        except NoSuchElementException as e:
-            print(e)
-            print("Could not find the element that contained all items")
+        #Re get the header cause the page will have reloaded
+        header = self.myDriver.find_element_by_tag_name("header")
+        #allItems = header.find_elements_by_xpath(self.itemElementXpath)
+        time.sleep(4)
 
-        try:
-            allItems = itemElement.find_elements_by_xpath(self.itemElementXpath)
-        except NoSuchElementException as e:
-            print(e)
-            print("Individual elements fails")
+        #print(len(allItems))
 
-        print(allItems)
+        itemNamePriceDict = {}
+        for num in range(1, 60):
+            for tries in range(self.maxRetries):
+                try:
+                    itemSpecificString = self.itemElementXpath+str(num)+"']"
+
+                    price = float(header.find_element_by_xpath(itemSpecificString+self.itemPriceXpath).text.split(" to ")[0][1:])
+                    name = header.find_element_by_xpath(itemSpecificString+self.itemNameXpath).text
+
+                    itemNamePriceDict[name] = price
+                    time.sleep(3)
+                    break
+                except Exception as e:
+                    print(e)
+                    time.sleep(3)
+
+        sortedItems = sorted(itemNamePriceDict.items(), key=lambda item: item[1])
+        sortedDict = {}
+        for item in sortedItems:
+            sortedDict[item[0]] = item[1]
+        return sortedDict
 
     def finish(self):
         self.myDriver.close()
